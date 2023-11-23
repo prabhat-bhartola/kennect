a63c8f656ea4e25006f99882550569e39c76e086
Prabhat Bhartola <prabhatbhartola@gmail.com>
2023-11-23 06:30:01 +0530

    Added custom_text_field, css changes, added mui and formik

---
+++ src/web/src/app/login/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import {
+  Box,
+  Card,
+  CardContent,
+  DialogActions,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { Form, Formik } from "formik";
+
+import { useState } from "react";
+import { LoadingButton } from "@mui/lab";
+import KennectTextField from "@/common/components/kennect_text_field.component";
+
+export default function Login() {
+  const handleSubmit = () => {};
+
+  return (
+    <Card
+      sx={{
+        border: 1,
+        borderColor: "#808080",
+        borderRadius: "16px",
+        mx: { xs: "5%", md: "25%" },
+        mt: "10%",
+      }}
+    >
+      <CardContent>
+        <Box sx={{ padding: "2.5%", mb: "25px" }}>
+          <Typography
+            variant="h4"
+            sx={{ display: "flex", justifyContent: "center" }}
+          >
+            Login
+          </Typography>
+        </Box>
+
+        <Formik
+          enableReinitialize
+          initialValues={() => {}}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
+              <Stack sx={{ width: { xs: "100%", md: "50%" } }} spacing={2}>
+                <KennectTextField
+                  variant="outlined"
+                  isDisabled={false}
+                  label="Username"
+                  name="username"
+                  onChangeHandler={() => {}}
+                />
+                <KennectTextField
+                  variant="outlined"
+                  isDisabled={false}
+                  label="Password"
+                  name="password"
+                  onChangeHandler={() => {}}
+                />
+              </Stack>
+            </Box>
+            <DialogActions sx={{ pt: "25px" }}>
+              <LoadingButton
+                variant="contained"
+                content="Save"
+                size="large"
+                type="submit"
+                disabled={false}
+                loading={false}
+              >
+                {"Login"}
+              </LoadingButton>
+            </DialogActions>
+          </Form>
+        </Formik>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/web/src/common/components/kennect_text_field.component.tsx
@@ -0,0 +1,50 @@
+import { SxProps, TextField, TextFieldVariants } from "@mui/material";
+import { useField } from "formik";
+
+type Props = {
+  variant: TextFieldVariants;
+  name: string;
+  label: string;
+  sx?: SxProps;
+  isDisabled: boolean;
+  fullWidth: boolean;
+  multiline: boolean;
+  rows: number;
+  onChangeHandler?: () => void;
+};
+
+function KennectTextField({
+  variant,
+  name,
+  label,
+  sx,
+  isDisabled,
+  fullWidth,
+  multiline,
+  rows,
+  onChangeHandler,
+}: Props) {
+  const [field, meta] = useField(name);
+  return (
+    <TextField
+      fullWidth={fullWidth}
+      multiline={multiline}
+      variant={variant}
+      label={label}
+      sx={sx}
+      disabled={isDisabled}
+      rows={rows}
+      onChangeCapture={onChangeHandler}
+      {...field}
+    />
+  );
+}
+
+KennectTextField.defaultProps = {
+  fullWidth: false,
+  multiline: false,
+  rows: 1,
+  onChangeHandler: () => {},
+};
+
+export default KennectTextField;
